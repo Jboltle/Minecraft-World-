20cb3000f78fec5775e0655f88d8c1ecf5787021
Jboltle <stacbolt2@gmail.com>
2023-12-19 18:56:09 -0600

    MinecraftWorld

---
+++ MinecraftWorld.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+import time
+from datetime import datetime
+
+# GitHub App details
+APP_ID = "722562"
+CLIENT_ID = "Iv1.8343edfd38855a86"
+PRIVATE_KEY_PATH = "/path/to/your/private-key.pem"
+REPO_NAME = "Minecraft-World-"
+
+# Local Minecraft server directory
+SERVER_DIR = "/path/to/your/minecraft/server"
+
+def commit_and_push_changes():
+    # Commit and push changes to the local Git repository
+    subprocess.run(["git", "-C", SERVER_DIR, "add", "."])
+    subprocess.run(["git", "-C", SERVER_DIR, "commit", "-m", "Automated commit - {}".format(datetime.now())])
+    subprocess.run(["git", "-C", SERVER_DIR, "push"])
+
+def push_to_github():
+    # Authenticate with GitHub App using the private key
+    subprocess.run(["gh", "auth", "login", "--with-token"], input=open(PRIVATE_KEY_PATH).read().encode(), check=True)
+
+    # Push changes to the GitHub repository
+    subprocess.run(["gh", "repo", "clone", REPO_NAME])
+    subprocess.run(["cp", "-r", SERVER_DIR + "/*", REPO_NAME])
+    subprocess.run(["gh", "repo", "upload", "--repo", REPO_NAME, "--branch", "main"])
+
+def main():
+    try:
+        while True:
+            # Commit and push changes every hour
+            commit_and_push_changes()
+            push_to_github()
+            time.sleep(3600)  # Sleep for one hour
+
+    except KeyboardInterrupt:
+        # Handle manual termination and push changes
+        commit_and_push_changes()
+        push_to_github()
+
+if __name__ == "__main__":
+    main()
