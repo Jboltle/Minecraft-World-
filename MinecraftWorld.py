d19b140aa7f837d90bafcce4b8da380d10a02ba2
Jboltle <stacbolt2@gmail.com>
2023-12-19 20:13:01 -0600

    Minecraft Automation

--- MinecraftWorld.py
+++ MinecraftWorld.py
@@ -6,11 +6,11 @@ from datetime import datetime
 # GitHub App details
 APP_ID = "722562"
 CLIENT_ID = "Iv1.8343edfd38855a86"
-PRIVATE_KEY_PATH = "/path/to/your/private-key.pem"
-REPO = "Minecraft-World-"
+PRIVATE_KEY_PATH = "C:/Users/Jon/Desktop/Minecraft-World-/minecraft-world-autouploader.2023-12-19.private-key.pem"
+REPO_NAME = "Minecraft-World-"
 
 # Local Minecraft server directory
-SERVER_DIR = "/path/to/your/minecraft/server"
+SERVER_DIR = "C:/Users/Jon/Desktop/Minecraft-World-/Minecraft Server"
 
 def commit_and_push_changes():
     # Commit and push changes to the local Git repository
@@ -23,9 +23,9 @@ def push_to_github():
     subprocess.run(["gh", "auth", "login", "--with-token"], input=open(PRIVATE_KEY_PATH).read().encode(), check=True)
 
     # Push changes to the GitHub repository
-    subprocess.run(["gh", "repo", "clone", REPO])
-    subprocess.run(["cp", "-r", SERVER_DIR + "/*", REPO])
-    subprocess.run(["gh", "repo", "upload", "--repo", REPO, "--branch", "main"])
+    subprocess.run(["gh", "repo", "clone", REPO_NAME])
+    subprocess.run(["cp", "-r", SERVER_DIR + "/*", REPO_NAME])
+    subprocess.run(["gh", "repo", "upload", "--repo", REPO_NAME, "--branch", "main"])
 
 def main():
     try:
@@ -41,4 +41,11 @@ def main():
         push_to_github()
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # Ensure changes are pushed before exiting
+        commit_and_push_changes()
+        push_to_github()
